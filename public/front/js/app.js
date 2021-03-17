1b8210f4c668d1811f98a5a18e97f603b395a03d
Dagmara Gryka <dagagryka@gmail.com>
2021-03-17 13:31:48 +0100

    add files, connect projects

---
+++ public/front/js/app.js
@@ -0,0 +1,152 @@
+import {settings,select,classNames} from './settings.js';
+import Product from './components/Product.js';
+import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
+import Home from './components/Home.js';
+
+const app = {
+
+  initBooking: function(){
+    const thisApp = this;
+
+    thisApp.bookingContainer = document.querySelector(select.containerOf.booking); //znajdowała kontener widgetu do rezerwacji stron
+    new Booking(thisApp.bookingContainer); //tworzyła nową instancję klasy,przekazywała do konstruktora kontener
+
+  },
+
+  initHome: function(){
+    const thisApp = this;
+    thisApp.homeWrapper = document.querySelector(select.containerOf.home);
+    new Home(thisApp.homeWrapper);
+  },
+
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children; //dzieci kontera stron
+    thisApp.homeLinks = document.querySelectorAll(select.nav.homeLinks);
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    //thisApp.orderBox = document.querySelector(select.widgets.home.orderBox);
+    //thisApp.bookBox = document.querySelector(select.widgets.home.bookBox);
+
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    const navs = [...thisApp.homeLinks, ...thisApp.navLinks ]; // O TUTAJ!!! ????
+
+    for(let nav of navs){
+      nav.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        thisApp.activatePage(id);
+        window.location.hash = '#/' + id;
+      });
+    }
+
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    //add class "active" to matching pages, remove from non matching
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    //add class "active" to matching links, remove from non matching
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+
+
+
+  },
+
+  initMenu: function(){ //instancje klasy Product // uruchamia sie jako druga
+    const thisApp = this; //  korzysta z przygotowanej wcześniej referencji do danych (thisApp.data)
+
+    //console.log('thisApp.data:', thisApp.data);
+
+    for(let productData in thisApp.data.products){
+      //new Product(productData, thisApp.data.products[productData]);
+      new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+    }
+    /*const testProduct = new Product();
+    console.log('testProduct:', testProduct);*/
+  },
+
+  initData: function(){ //aplikacja korzystała z tego źródła danych data.js
+    const thisApp = this;
+
+    //thisApp.data = dataSource;
+    thisApp.data = {};
+
+    const url = settings.db.url + '/' + settings.db.product; //http://localhost:3131/product
+
+    fetch(url) //funkcji fetch wysyłamy zapytanie (request) pod podany adres endpointu
+      .then(function(rawResponse){ //otrzyma odpowiedź jest w formacie JSON
+        return rawResponse.json(); //skonwertuj dane do obiektu JS-owego.
+      })
+
+      .then(function(parsedResponse){ //pokaż w konsoli te skonwertowane dane.
+        //console.log('parsedResponse', parsedResponse);
+
+        //save parsedResponse at thisApp.data.products
+        thisApp.data.products = parsedResponse;
+
+        //execute initMenu menthod
+        thisApp.initMenu();
+
+      });
+
+    console.log('thisApp.data',JSON.stringify(thisApp.data));
+  },
+
+  init: function(){ // Ta wywołuje dwie kolejne – initData i initMenu
+    const thisApp = this;
+    //console.log('*** App starting ***');
+    //console.log('thisApp:', thisApp);
+    //console.log('classNames:', classNames);
+    //console.log('settings:', settings);
+    //console.log('templates:', templates);
+
+    thisApp.initHome();
+
+    thisApp.initPages();
+    thisApp.initData();
+    thisApp.initCart();
+
+    thisApp.initBooking();
+  },
+
+  initCart: function(){ //inicjowała instancję koszyka.
+    const thisApp = this;
+
+    const cartElem = document.querySelector(select.containerOf.cart);
+    thisApp.cart = new Cart(cartElem);
+
+    thisApp.productList = document.querySelector(select.containerOf.menu);
+
+    thisApp.productList.addEventListener('add-to-cart', function(event){
+      app.cart.add(event.detail.product);
+    });
+  },
+};
+
+app.init();
---
+++ public/front/js/components/AmountWidget.js
@@ -0,0 +1,66 @@
+import {select, settings} from '../settings.js';
+import BaseWidget from './BaseWidget.js';
+
+class AmountWidget extends BaseWidget{ //dziedziczy metody z basewidget
+  constructor(element){
+    super(element, settings.amountWidget.defaultValue);
+
+    const thisWidget = this;
+
+    thisWidget.getElements(element);
+    thisWidget.initActions();
+
+    thisWidget.value = thisWidget.dom.input.value;
+  }
+
+  getElements(){
+    const thisWidget = this;
+
+    //thisWidget.element = element;
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
+    thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
+
+    //thisWidget.dom.input.value = thisWidget.dom.wrapper.querySelector(select.widgets.amount);
+  }
+
+  isValid(value){
+    return !isNaN(value)
+      && value >= 0
+      && value <= 10;
+
+  }
+
+  renderValue(){ // zeby wartosc widget wyswietlila sie na stronie
+    const thisWidget = this;
+
+    thisWidget.dom.input.value = thisWidget.value;
+
+  }
+
+  initActions(){
+    const thisWidget = this;
+
+    thisWidget.dom.input.addEventListener('change', function(){
+      //thisWidget.setValue(thisWidget.dom.input.value);
+      thisWidget.value = thisWidget.dom.input.value;
+    });
+
+    thisWidget.dom.linkDecrease.addEventListener('click',function(event){
+      event.preventDefault();
+      thisWidget.value = thisWidget.value - 1;
+    });
+
+    thisWidget.dom.linkIncrease.addEventListener('click', function(event){
+      event.preventDefault();
+      thisWidget.value = thisWidget.value + 1;
+    });
+    // DO POPRAWKI AMOUNT W ORDER!!!!!!!!!!
+  }
+
+
+
+
+}
+
+export default AmountWidget;
