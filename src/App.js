26164df4e0309ceb88dac38d01371a8ea33a3571
Dagmara Gryka <dagagryka@gmail.com>
2021-03-18 05:29:23 +0100

    routing and components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MainLayout from "./components/layout/MainLayout/MainLayout";
+import Login from '../src/components/views/Login/Login';
+import Kitchen from '../src/components/views/Kitchen/Kitchen';
+import Tables from '../src/components/views/Tables/Tables';
+import Ordering from '../src/components/views/Ordering/Ordering';
+import Dashboard from '../src/components/views/Dashboard/Dashboard';
 
 function App() {
   return (
-    <MainLayout>
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    </MainLayout>
+    <BrowserRouter basename={'/panel'}>
+      <MainLayout>
+        <Switch>
+          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard} />
+          <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
+          <Route exact path={process.env.PUBLIC_URL + '/ordering'} component={Ordering} />
+          <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+        </Switch>
+      </MainLayout>
+    </BrowserRouter>
   );
 }
 
--- src/components/views/Ordering/Ordering.js
+++ src/components/views/Ordering/Ordering.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import styles from './Ordering.scss';
+import { Link } from 'react-router-dom';
 
 const Ordering = () => (
   <div className={styles.component}>
     <h2>Ordering view</h2>
+    <Link to={`${process.env.PUBLIC_URL}/ordering/booking/new`}> new order </Link>
+    <Link to={`${process.env.PUBLIC_URL}/ordering/booking/new`}> order id </Link>
   </div>
 );
 
