6fcfff74b5b2abe8c94c6933a0ae5bddce2f6915
Dagmara Gryka <dagagryka@gmail.com>
2021-03-23 15:49:37 +0100

    fix view

--- src/components/views/Ordering/Ordering.js
+++ src/components/views/Ordering/Ordering.js
@@ -9,12 +9,12 @@ import { Table, TableHead, TableRow, TableCell, TableBody, } from '@material-ui/
 
 
 const rows = [ // ? 
-  { id: 1, tableId: '1', name: 'ORDER', status: 'READY', orderTime: '13:30', summary: '40' },
-  { id: 2, tableId: '2', name: 'ORDER', status: 'DELIVERED', orderTime: '10:00', summary: '35' },
-  { id: 3, tableId: '3', name:  'ORDER', status: 'READY', orderTime: '13:30', summary: '100' },
-  { id: 4, tableId: '4', name: 'ORDER', status: 'ORDERING', orderTime: '13:45', summary: '220' },
-  { id: 5, tableId: '5', name: 'ORDER', status: 'DELIVERED', orderTime: '16:00', summary: '250' },
-  { id: 6, tableId: '6', name: 'ORDER', status: 'READY', orderTime: '13:55', summary: '220' },
+  { tableId: '1', name: 'ORDER', status: 'READY', orderTime: '13:30', summary: '40' },
+  { tableId: '2', name: 'ORDER', status: 'DELIVERED', orderTime: '10:00', summary: '35' },
+  { tableId: '3', name:  'ORDER', status: 'READY', orderTime: '13:30', summary: '100' },
+  { tableId: '4', name: 'ORDER', status: 'ORDERING', orderTime: '13:45', summary: '220' },
+  { tableId: '5', name: 'ORDER', status: 'DELIVERED', orderTime: '16:00', summary: '250' },
+  { tableId: '6', name: 'ORDER', status: 'READY', orderTime: '13:55', summary: '220' },
   
 ];
 
@@ -42,7 +42,7 @@ const Ordering = () => (
           </TableHead>
           <TableBody>
             {rows.map((row) => (
-              <TableRow key={row.id} style={`${row.status}` === 'READY' ? { background: '#e91e63' } : { background: '#ed4b82' }}>
+              <TableRow key={row.tableId} style={`${row.status}` === 'READY' ? { background: '#e91e63' } : { background: '#ed4b82' }}>
                 <TableCell align="left">
                   <Button variant="contained" color="primary" size="small">
                     <Link to={process.env.PUBLIC_URL + '/ordering/new'} style={{ textDecoration: 'none'}}>
