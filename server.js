7e3b7dde7b2b0fba1417260495f5016ca2602de0
Dagmara Gryka <dagagryka@gmail.com>
2021-03-24 18:54:11 +0100

    fix page

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('build/db/app.json');
 const middlewares = jsonServer.defaults({
-  static: './',
-  noCors: true
+  static: 'build'
 });
 const port = process.env.PORT || 3131;
 
